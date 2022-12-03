6c5661d2fb4609221351ba1341fd166c17da4759
KrisnaBotzOfc <117555820+KrisnaBotzOfc@users.noreply.github.com>
2022-12-03 11:40:51 +0700

    Update jarot.js

--- jarot.js
+++ jarot.js
@@ -44,7 +44,7 @@ const xtime = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
 const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss')  
  if(time2 < "23:59:00"){
-var ucapanWaktu = 'Good night 🌌'
+var ucapanWaktu = 'SelamAAt malam 🌌'
  }
  if(time2 < "19:00:00"){
 var ucapanWaktu = 'Good afternoon 🌃'
